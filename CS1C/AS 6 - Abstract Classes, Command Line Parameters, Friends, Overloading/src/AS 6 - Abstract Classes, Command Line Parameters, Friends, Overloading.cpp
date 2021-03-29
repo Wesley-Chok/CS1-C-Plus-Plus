045acb158588441aa1e4811532ca07f9277e1705
Wesley-Chok <55215498+Wesley-Chok@users.noreply.github.com>
2021-03-28 23:59:38 -0700

    Add files via upload

---
+++ CS1C/AS 6 - Abstract Classes, Command Line Parameters, Friends, Overloading/src/AS 6 - Abstract Classes, Command Line Parameters, Friends, Overloading.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : AS.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include "Shape.h"
+
+int main() 
+{
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
---
+++ CS1C/AS 6 - Abstract Classes, Command Line Parameters, Friends, Overloading/src/Shape.h
@@ -0,0 +1,15 @@
+#ifndef SHAPE_H_
+#define SHAPE_H_
+
+#include <iostream>
+using namespace std;
+
+class Shape
+{
+	
+};
+
+
+
+
+#endif /* SHAPE_H_ */
